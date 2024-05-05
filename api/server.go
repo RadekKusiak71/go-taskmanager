694a8c4506aeb9c150406197232a3f7c049d99b8
RadekKusiak71 <radoslawkusiak12@gmail.com>
2024-05-05 14:11:29 +0200

    writeJSON + err handling

--- api/server.go
+++ api/server.go
@@ -31,61 +31,66 @@ func NewAPIServer(addr string, db *db.PostgresDB) *APIServer {
 func (s *APIServer) Run() error {
 	router := mux.NewRouter()
 
-	router.HandleFunc("/tasks/{id}/", s.getTaskById).Methods("GET")
-	router.HandleFunc("/tasks/{id}/", s.deleteTaskById).Methods("DELETE")
-	router.HandleFunc("/tasks/user/{id}/", s.getTaskForUser)
-	router.HandleFunc("/tasks/", s.getTasks).Methods("GET")
-	router.HandleFunc("/tasks/", s.createTask).Methods("POST")
-	router.HandleFunc("/tasks/{id}/", s.updateTask).Methods("PUT")
-	router.HandleFunc("/register/", s.registerUser).Methods("POST")
-	router.HandleFunc("/login/", s.loginCustomer).Methods("POST")
+	router.HandleFunc("/tasks/{id}/", makeHTTPHandleFunc(s.getTaskById)).Methods("GET")
+	router.HandleFunc("/tasks/{id}/", makeHTTPHandleFunc(s.deleteTaskById)).Methods("DELETE")
+	router.HandleFunc("/tasks/user/{id}/", makeHTTPHandleFunc(s.getTaskForUser))
+	router.HandleFunc("/tasks/", makeHTTPHandleFunc(s.getTasks)).Methods("GET")
+	router.HandleFunc("/tasks/", makeHTTPHandleFunc(s.createTask)).Methods("POST")
+	router.HandleFunc("/tasks/{id}/", makeHTTPHandleFunc(s.updateTask)).Methods("PUT")
+	router.HandleFunc("/register/", makeHTTPHandleFunc(s.registerUser)).Methods("POST")
+	router.HandleFunc("/login/", makeHTTPHandleFunc(s.loginCustomer)).Methods("POST")
 
 	log.Printf("API server running on port: %s", s.addr)
 	return http.ListenAndServe(s.addr, router)
 }
 
-func (s *APIServer) getTaskById(w http.ResponseWriter, r *http.Request) {
-	taskID := mux.Vars(r)["id"]
+func (s *APIServer) getTaskById(w http.ResponseWriter, r *http.Request) error {
+	taskID, err := getID("id", r)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
 	task, err := s.db.GetTaskByID(taskID)
 	if err != nil {
 		log.Println(err)
-		return
+		return err
 	}
-	json.NewEncoder(w).Encode(task)
+	return writeJSON(w, http.StatusOK, task)
 }
 
-func (s *APIServer) getTasks(w http.ResponseWriter, r *http.Request) {
+func (s *APIServer) getTasks(w http.ResponseWriter, r *http.Request) error {
 	tasks, err := s.db.GetTasks()
 	if err != nil {
 		log.Println(err)
-		return
+		return err
 	}
-	json.NewEncoder(w).Encode(tasks)
+	return writeJSON(w, http.StatusOK, tasks)
 }
-func (s *APIServer) getTaskForUser(w http.ResponseWriter, r *http.Request) {
+func (s *APIServer) getTaskForUser(w http.ResponseWriter, r *http.Request) error {
 	userID := mux.Vars(r)["id"]
 
 	id, err := strconv.Atoi(userID)
 
 	if err != nil {
 		log.Println(err)
-		return
+		return err
 	}
 	tasks, err := s.db.GetTaskByUserID(id)
 	if err != nil {
 		log.Println(err)
-		return
+		return err
 	}
-	json.NewEncoder(w).Encode(tasks)
+	return writeJSON(w, http.StatusOK, tasks)
 }
 
-func (s *APIServer) createTask(w http.ResponseWriter, r *http.Request) {
+func (s *APIServer) createTask(w http.ResponseWriter, r *http.Request) error {
 	requestBody := r.Body
 
 	bs, err := io.ReadAll(requestBody)
 	if err != nil {
 		log.Println(err)
-		return
+		return err
 	}
 
 	var newCreateTask types.CreateTask
@@ -97,36 +102,44 @@ func (s *APIServer) createTask(w http.ResponseWriter, r *http.Request) {
 
 	if err != nil {
 		log.Println(err)
-		return
+		return err
 	}
 
-	fmt.Println(newCreateTask)
 	err = s.db.CreateTask(newCreateTask)
 	if err != nil {
 		log.Println(err)
-		return
+		return err
 	}
-	json.NewEncoder(w).Encode(newCreateTask)
+	return writeJSON(w, http.StatusCreated, newCreateTask)
 }
 
-func (s *APIServer) deleteTaskById(w http.ResponseWriter, r *http.Request) {
-	taskID := mux.Vars(r)["id"]
-
-	err := s.db.DeleteByTaskID(taskID)
+func (s *APIServer) deleteTaskById(w http.ResponseWriter, r *http.Request) error {
+	taskID, err := getID("id", r)
 	if err != nil {
 		log.Println(err)
-		return
+		return err
 	}
-	json.NewEncoder(w).Encode(map[string]string{"message": "Task deleted successfully"})
+
+	err = s.db.DeleteByTaskID(taskID)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	return writeJSON(w, http.StatusOK, map[string]string{"message": "Task deleted successfully"})
 }
 
-func (s *APIServer) updateTask(w http.ResponseWriter, r *http.Request) {
-	taskID := mux.Vars(r)["id"]
+func (s *APIServer) updateTask(w http.ResponseWriter, r *http.Request) error {
+	taskID, err := getID("id", r)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
 	bs, err := io.ReadAll(r.Body)
 
 	if err != nil {
 		log.Println(err)
-		return
+		return err
 	}
 
 	var updateValues types.UpdateTask
@@ -134,24 +147,24 @@ func (s *APIServer) updateTask(w http.ResponseWriter, r *http.Request) {
 
 	if err != nil {
 		log.Println(err)
-		return
+		return err
 	}
 
 	task, err := s.db.UpdateTask(taskID, updateValues)
 
 	if err != nil {
 		log.Println(err)
-		return
+		return err
 	}
-	json.NewEncoder(w).Encode(task)
+	return writeJSON(w, http.StatusOK, task)
 }
 
-func (s *APIServer) registerUser(w http.ResponseWriter, r *http.Request) {
+func (s *APIServer) registerUser(w http.ResponseWriter, r *http.Request) error {
 	bs, err := io.ReadAll(r.Body)
 
 	if err != nil {
 		log.Println(err)
-		return
+		return err
 	}
 
 	var newCustomer types.RegisterRequest
@@ -159,27 +172,27 @@ func (s *APIServer) registerUser(w http.ResponseWriter, r *http.Request) {
 	err = json.Unmarshal(bs, &newCustomer)
 	if err != nil {
 		log.Println(err)
-		return
+		return err
 	}
 
 	err = newCustomer.CryptPassword()
 	if err != nil {
 		log.Println(err)
-		return
+		return err
 	}
 	err = s.db.CreateCustomer(newCustomer)
 	if err != nil {
 		log.Println(err)
-		return
+		return err
 	}
-	json.NewEncoder(w).Encode(map[string]string{"message": "User created"})
+	return writeJSON(w, http.StatusCreated, map[string]string{"message": "User created"})
 }
 
-func (s *APIServer) loginCustomer(w http.ResponseWriter, r *http.Request) {
+func (s *APIServer) loginCustomer(w http.ResponseWriter, r *http.Request) error {
 	bs, err := io.ReadAll(r.Body)
 	if err != nil {
 		log.Println(err)
-		return
+		return err
 	}
 
 	var loginRequest types.LoginRequest
@@ -188,20 +201,43 @@ func (s *APIServer) loginCustomer(w http.ResponseWriter, r *http.Request) {
 
 	if err != nil {
 		log.Println(err)
-		return
+		return err
 	}
 	customer, err := s.db.GetCustomerByUsername(loginRequest.Username)
 
 	if err != nil {
 		log.Println(err)
-		return
+		return err
 	}
 
 	if err = bcrypt.CompareHashAndPassword([]byte(customer.Password), []byte(loginRequest.Password)); err != nil {
 		log.Println(err)
-		return
+		return err
 	}
 
 	log.Println("Login successfull")
-	json.NewEncoder(w).Encode(map[string]string{"sessionid": uuid.NewV4().String()})
+	return writeJSON(w, http.StatusCreated, map[string]string{"sessionid": uuid.NewV4().String()})
+}
+
+func getID(name string, r *http.Request) (string, error) {
+	if key, ok := mux.Vars(r)[name]; ok {
+		return key, nil
+	}
+	return "", fmt.Errorf("key was not found: %s", name)
+}
+
+func writeJSON(w http.ResponseWriter, status int, v any) error {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	return json.NewEncoder(w).Encode(v)
+}
+
+type apiFunc func(w http.ResponseWriter, r *http.Request) error
+
+func makeHTTPHandleFunc(f apiFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := f(w, r); err != nil {
+			writeJSON(w, http.StatusBadRequest, err)
+		}
+	}
 }
